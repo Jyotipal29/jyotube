b574f20075b52524d276d54fc65827879e5a0f16
jyoti pal <paljyoti0129@gmail.com>
2023-06-09 22:53:57 +0530

    updated togglelike function

--- backend/controller/likeController.js
+++ backend/controller/likeController.js
@@ -3,30 +3,31 @@ const Like = require("../model/likeModel");
 
 // Route for toggling the like status of a video
 const toggleLike = async (req, res) => {
-  const { videoId } = req.params;
+  const { id } = req.params;
 
   try {
-    const video = await Video.findById(videoId);
+    const video = await Video.findById(id);
     if (!video) {
       return res.status(404).json({ error: "Video not found" });
     }
 
     // Find the like associated with the user and the video
-    const like = await Like.findOne({ user: req.user, video: videoId });
+    const like = await Like.findOne({ user: req.user, video: id });
 
     if (like) {
       // If the user already liked the video, remove the like
+      await Like.findByIdAndRemove(like._id);
       video.likes.pull(like._id);
       await video.save();
-      await like.remove();
-      return res.json({ message: "Video unliked successfully",video,like });
+      //   await like.remove();
+      return res.json({ message: "Video unliked successfully", video });
     } else {
       // If the user hasn't liked the video, create a new like
-      const newLike = new Like({ user: req.user, video: videoId });
+      const newLike = new Like({ user: req.user, video: id });
       await newLike.save();
       video.likes.push(newLike);
       await video.save();
-      return res.json({ message: "Video liked successfully", video, like });
+      return res.json({ message: "Video liked successfully", video });
     }
   } catch (error) {
     console.error("Error toggling like status:", error);
