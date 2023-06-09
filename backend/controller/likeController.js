a68ae09f9af30c6ff033da4b1590006c24d9bfe2
jyoti pal <paljyoti0129@gmail.com>
2023-06-09 22:29:38 +0530

    created toggle like function

---
+++ backend/controller/likeController.js
@@ -0,0 +1,39 @@
+const Video = require("../model/videoModel");
+const Like = require("../model/likeModel");
+
+// Route for toggling the like status of a video
+const togelLike = async (req, res) => {
+  const { videoId } = req.params;
+
+  try {
+    const video = await Video.findById(videoId);
+    if (!video) {
+      return res.status(404).json({ error: "Video not found" });
+    }
+
+    // Find the like associated with the user and the video
+    const like = await Like.findOne({ user: req.user, video: videoId });
+
+    if (like) {
+      // If the user already liked the video, remove the like
+      video.likes.pull(like._id);
+      await video.save();
+      await like.remove();
+      return res.json({ message: "Video unliked successfully" });
+    } else {
+      // If the user hasn't liked the video, create a new like
+      const newLike = new Like({ user: req.user, video: videoId });
+      await newLike.save();
+      video.likes.push(newLike);
+      await video.save();
+      return res.json({ message: "Video liked successfully" });
+    }
+  } catch (error) {
+    console.error("Error toggling like status:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = {
+  togelLike,
+};
