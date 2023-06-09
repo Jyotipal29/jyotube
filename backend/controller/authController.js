aaa5e5880fba0b08ec5db07940f9c5e4cc15c626
jyoti pal <paljyoti0129@gmail.com>
2023-06-09 21:32:17 +0530

    fixed user model in user controller

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const User = require("../models/User");
+const User = require("../model/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
 connectDB();
+
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`listening in port number ${port}`));
