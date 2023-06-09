c38a1cab8b9725264fac000ae3fce29732277ecd
jyoti pal <paljyoti0129@gmail.com>
2023-06-09 20:52:25 +0530

    created video model

---
+++ backend/model/videoModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  thumbnailUrl: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Like",
+    },
+  ],
+  watchlater: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Watchlater",
+    },
+  ],
+
+  tag: {
+    type: [String],
+    required: true,
+  },
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = Video;
