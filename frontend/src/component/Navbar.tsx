134f805740292ac89f62fd63ac5c6f06775ac831
jyoti pal <paljyoti0129@gmail.com>
2023-06-10 20:25:40 +0530

    added login register links

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
-
+import { useUser } from "../context/userContext/userContext";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { userState } = useUser();
+  const logoutHandler = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
   return (
     <div className="bg-gray-900">
       <div className="container mx-auto px-8 flex justify-between items-center py-3">
@@ -14,6 +21,18 @@ const Navbar = () => {
           <li>
             <Link to="/watchlater">watchlater</Link>
           </li>
+          {userState?.user?.token ? (
+            <>
+              <p className="border-2 rounded-full px-2 text-yellow-400 border-yellow-400">
+                {userState?.user?.name?.charAt(0)}
+              </p>
+              <button onClick={logoutHandler}>logout</button>
+            </>
+          ) : (
+            <button className="bg-black text-white py-1 px-3 rounded-md">
+              <Link to="/login">login</Link>
+            </button>
+          )}
         </ul>
       </div>
     </div>
