7e1ebe034c00b1c384fd8f038caed44c5e7bce37
jyoti pal <paljyoti0129@gmail.com>
2023-06-10 19:23:18 +0530

    style updated

--- frontend/src/component/VideoCard.tsx
+++ frontend/src/component/VideoCard.tsx
@@ -1,15 +1,31 @@
+import { Link } from "react-router-dom";
+
 const VideoCard = (item: Video) => {
   console.log(item, "item");
   return (
-    <div className="">
-      <img
-        src={item.thumbnailUrl}
-        alt="video"
-        className="w-80 h-60 rounded-lg"
-      />
-      <p className="text-xl font-bold">{item.title}</p>
+    <div className=" flex flex-col space-y-2">
+      <div className="cursor-pointer">
+        <Link to={`/video/${item._id}`}>
+          <img
+            src={item.thumbnailUrl}
+            alt="video"
+            className="w-80 h-60 rounded-lg"
+          />
+        </Link>
+      </div>
 
-      <h1 className="text-md text-slate-500 uppercase">{item.creator}</h1>
+      <div className="flex justify-start px-2 space-x-2">
+        <div className="w-10 ">
+          <img src={item.channelImg} alt="" className=" w-full  rounded-full" />
+        </div>
+        <div className="w-40 ">
+          <p className="text-md font-bold">{item.title}</p>
+
+          <h1 className="text-sm font-semibold uppercase text-gray-600">
+            {item.creator}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
