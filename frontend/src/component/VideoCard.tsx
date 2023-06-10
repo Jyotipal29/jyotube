3b289e1f7b014e8a4edf44de326920bc569c0b9c
jyoti pal <paljyoti0129@gmail.com>
2023-06-10 15:03:28 +0530

    style updated

--- frontend/src/component/VideoCard.tsx
+++ frontend/src/component/VideoCard.tsx
@@ -1,12 +1,15 @@
 const VideoCard = (item: Video) => {
   console.log(item, "item");
   return (
-    <div>
-      <div>
-        <img src={item.thumbnailUrl} alt="video" className="w-80" />
-        <h1>{item.creator}</h1>
-        <p>{item.title}</p>
-      </div>
+    <div className="">
+      <img
+        src={item.thumbnailUrl}
+        alt="video"
+        className="w-80 h-60 rounded-lg"
+      />
+      <p className="text-xl font-bold">{item.title}</p>
+
+      <h1 className="text-md text-slate-500 uppercase">{item.creator}</h1>
     </div>
   );
 };
