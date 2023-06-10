610adc394d38b72164fde631c3012a57adfd0bdd
jyoti pal <paljyoti0129@gmail.com>
2023-06-10 15:03:38 +0530

    style updated

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Videos from "./Videos";
 
 const Home = () => {
-  return (
-    <div>
-      <Videos />
-    </div>
-  );
+    return (
+      <div className=" py-8">
+        <div className="container mx-auto  px-8">
+          <Videos />
+        </div>
+      </div>
+    );
 };
 
 export default Home;
