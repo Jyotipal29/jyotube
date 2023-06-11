05d0167f38689e596a472c529b485c4f4004dda3
jyoti pal <paljyoti0129@gmail.com>
2023-06-11 12:47:52 +0530

    created get watchlater videos routes

--- backend/routes/watchlaterRoutes.js
+++ backend/routes/watchlaterRoutes.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const { protect } = require("./verifyToken");
-const { toggleWatchlater } = require("../controller/watchlaterController");
+const {
+  toggleWatchlater,
+  getWatchlaterVideos,
+} = require("../controller/watchlaterController");
 
 router.post("/toggel/:id", protect, toggleWatchlater);
+router.get("/", protect, getWatchlaterVideos);
 
 module.exports = router;
