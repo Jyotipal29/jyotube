357b07b2f730d575fbc230bc886e88f61edaedfd
jyoti pal <paljyoti0129@gmail.com>
2023-06-11 13:03:17 +0530

    created watchlater page

--- frontend/src/pages/Watchlater.tsx
+++ frontend/src/pages/Watchlater.tsx
@@ -1,5 +1,36 @@
+import axios from "axios";
+import { api } from "../constant/api";
+import { useEffect, useState } from "react";
+import { useUser } from "../context/userContext/userContext";
+import VideoCard from "../component/VideoCard";
+
 const Watchlater = () => {
-  return <div>Watchlater</div>;
+  const [watchLater, setWatchLater] = useState([]);
+  const {
+    userState: { user },
+  } = useUser();
+  const getWatchLaterVideos = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    };
+    const { data } = await axios.get(`${api}watchlater/`, config);
+    console.log(data.videos, "liked data");
+    setWatchLater(data.videos);
+  };
+
+  useEffect(() => {
+    getWatchLaterVideos();
+  }, []);
+  console.log(watchLater, "liked vidios");
+  return (
+    <div>
+      {watchLater.map((item: Video) => (
+        <VideoCard {...item} />
+      ))}
+    </div>
+  );
 };
 
 export default Watchlater;
