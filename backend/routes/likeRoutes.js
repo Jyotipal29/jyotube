434f28021dfb32d890cd45eb65b98bd0749ff034
jyoti pal <paljyoti0129@gmail.com>
2023-06-11 12:41:01 +0530

    added get likes videos routes

--- backend/routes/likeRoutes.js
+++ backend/routes/likeRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 const { protect } = require("./verifyToken");
-const { toggleLike } = require("../controller/likeController");
+const { toggleLike, getLikedVideos } = require("../controller/likeController");
 
 router.post("/toggel/:id", protect, toggleLike);
+router.post("/", protect, getLikedVideos);
 
 module.exports = router;
