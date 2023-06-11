e5af1ed1c99c064fa95f2ec9b7d000493ea9d4de
jyoti pal <paljyoti0129@gmail.com>
2023-06-11 17:55:27 +0530

    updated video page

--- frontend/src/pages/Video.tsx
+++ frontend/src/pages/Video.tsx
@@ -17,82 +17,86 @@ const Video = () => {
     userState: { user },
   } = useUser();
   const {
-    videoState: { video },
+    videoState: { video, liked },
     videoDispatch,
   } = useVideo();
   const getVideo = async () => {
     const { data } = await axios.get<Video>(`${api}video/find/${id}`);
     setCurrVideo(data);
     videoDispatch({ type: "GET_VIDEO", payload: data });
-    console.log(data, " one data");
   };
   useEffect(() => {
     getVideo();
   }, [id]);
 
-  
-    const likeHandler = async (id: string) => {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-        },
-      };
-
-      const { data } = await axios.post(`${api}like/toggel/${id}`, {}, config);
-      console.log(data, "data");
+  const likeHandler = async (id: string) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
     };
 
-    const watchHandler = async (id: string) => {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-        },
-      };
+    const { data } = await axios.post<Video>(
+      `${api}like/toggel/${id}`,
+      {},
+      config
+    );
+    videoDispatch({ type: "TOGGLE_LIKE", payload: data });
+    console.log(data, "like data");
+  };
 
-      const { data } = await axios.post(
-        `${api}watchlater/toggel/${id}`,
-        {},
-        config
-      );
-      console.log(data, "data");
+  const watchHandler = async (id: string) => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
     };
-    return (
-      <div className="container mx-auto px-6 mt-5 rounded-lg">
-        <div className="flex justify-center ">
-          <iframe
-            width="100%"
-            height="500px"
-            title="youtube video"
-            src={currVideo?.videoUrl}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+
+    const { data } = await axios.post(
+      `${api}watchlater/toggel/${id}`,
+      {},
+      config
+    );
+    console.log("watched");
+  };
+  const isVideoLiked =
+    currVideo && liked.some((item) => item._id === currVideo._id);
+  console.log(isVideoLiked);
+  return (
+    <div className="container mx-auto px-6 mt-5 rounded-lg">
+      <div className="flex justify-center ">
+        <iframe
+          width="100%"
+          height="500px"
+          title="youtube video"
+          src={currVideo?.videoUrl}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="bg-gray-900 flex justify-between items-center  px-2 py-3 ">
+        <div className="flex items-center">
+          <button onClick={() => currVideo?._id && likeHandler(currVideo?._id)}>
+            {isVideoLiked ? (
+              <AiFillLike className="text-white text-2xl md:text-4xl cursor-pointer" />
+            ) : (
+              <AiOutlineLike className="text-white text-2xl md:text-4xl cursor-pointer" />
+            )}
+          </button>
         </div>
-        <div className="bg-gray-900 flex justify-between items-center  px-2 py-3 ">
-          <div className="flex items-center">
-            <button
-              onClick={() => currVideo?._id && likeHandler(currVideo?._id)}
-            >
-              {currVideo?.likes.includes(currVideo?._id) ? (
-                <AiFillLike className="text-white text-2xl md:text-4xl cursor-pointer" />
-              ) : (
-                <AiOutlineLike className="text-white text-2xl md:text-4xl cursor-pointer" />
-              )}
-            </button>
-          </div>
-          <div className="flex items-center">
-            <MdOutlineWatchLater
-              className="text-white text-2xl md:text-4xl cursor-pointer"
-              onClick={() => currVideo?._id && watchHandler(currVideo?._id)}
-            />
-          </div>
-          <div className="flex items-center justify-center">
-            <CgPlayListAdd className="text-white text-2xl md:text-4xl cursor-pointer" />
-          </div>
+        <div className="flex items-center">
+          <MdOutlineWatchLater
+            className="text-white text-2xl md:text-4xl cursor-pointer"
+            onClick={() => currVideo?._id && watchHandler(currVideo?._id)}
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <CgPlayListAdd className="text-white text-2xl md:text-4xl cursor-pointer" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Video;
