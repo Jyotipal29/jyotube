e5b187e2b049e50bc2f369326048ba32f1362d33
jyoti pal <paljyoti0129@gmail.com>
2023-06-11 13:03:03 +0530

    created liked videos page

--- frontend/src/pages/Likes.tsx
+++ frontend/src/pages/Likes.tsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { api } from "../constant/api";
+import { useEffect, useState } from "react";
+import { useUser } from "../context/userContext/userContext";
+import VideoCard from "../component/VideoCard";
 const Likes = () => {
-  return <div>Likes</div>;
+  const [likedVideo, setLikedVideo] = useState([]);
+  const {
+    userState: { user },
+  } = useUser();
+  const getLikedVideos = async () => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+      },
+    };
+    const { data } = await axios.get(`${api}like/`, config);
+    console.log(data.videos, "liked data");
+    setLikedVideo(data.videos);
+  };
+
+  useEffect(() => {
+    getLikedVideos();
+  }, []);
+  console.log(likedVideo, "liked vidios");
+  return (
+    <div>
+      {likedVideo.map((item: Video) => (
+        <VideoCard {...item} />
+      ))}
+    </div>
+  );
 };
 
 export default Likes;
