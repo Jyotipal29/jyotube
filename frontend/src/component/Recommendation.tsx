99edecefc986f57b7fa37bb2ce49985ee3512e3e
jyoti pal <paljyoti0129@gmail.com>
2023-06-13 20:32:55 +0530

    feat: video recommendation

--- frontend/src/component/Recommendation.tsx
+++ frontend/src/component/Recommendation.tsx
@@ -1,5 +1,34 @@
-const Recommendation = () => {
-  return <div>Recommendation</div>;
+import axios from "axios";
+import { api } from "../constant/api";
+import { useVideo } from "../context/videoContext/videoContext";
+import { useUser } from "../context/userContext/userContext";
+import { useEffect } from "react";
+type Id = {
+  id: string;
+};
+const Recommendation = ({ id }: Id) => {
+  const {
+    videoState: { recommend },
+    videoDispatch,
+  } = useVideo();
+  const getVideo = async () => {
+    const { data } = await axios.get<Video[]>(`${api}video/rcmdt/${id}`);
+    videoDispatch({ type: "GET_RCMDT", payload: data });
+  };
+  useEffect(() => {
+    getVideo();
+  }, [id]);
+
+  console.log(recommend, "rec");
+  return (
+    <div>
+      {recommend.map((item) => (
+        <div>
+          <img src={item.thumbnailUrl} className="w-20" />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Recommendation;
--- frontend/src/pages/Video.tsx
+++ frontend/src/pages/Video.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { useVideo } from "../context/videoContext/videoContext";
 import { useUser } from "../context/userContext/userContext";
 import { useNavigate } from "react-router-dom";
+import Recommendation from "../component/Recommendation";
 const Video = () => {
   const { id } = useParams();
   const [currVideo, setCurrVideo] = useState<Video | null>(null);
@@ -118,51 +119,54 @@ const Video = () => {
     setNewPlaylistName("");
   };
   return (
-    <div className="container mx-auto px-6 mt-5 rounded-lg">
-      <div className="flex justify-center ">
-        <iframe
-          width="100%"
-          height="500px"
-          title="youtube video"
-          src={currVideo?.videoUrl}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div className="bg-gray-900 flex justify-between items-center  px-2 py-3 ">
-        <div className="flex items-center">
-          <button onClick={() => currVideo?._id && likeHandler(currVideo?._id)}>
-            {isVideoLiked ? (
-              <AiFillLike className="text-white text-2xl md:text-4xl cursor-pointer" />
-            ) : (
-              <AiOutlineLike className="text-white text-2xl md:text-4xl cursor-pointer" />
-            )}
-          </button>
+    <>
+      <div className="container mx-auto px-6 mt-5 rounded-lg">
+        <div className="flex justify-center ">
+          <iframe
+            width="100%"
+            height="500px"
+            title="youtube video"
+            src={currVideo?.videoUrl}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         </div>
-        <div className="flex items-center">
-          <button
-            onClick={() => currVideo?._id && watchHandler(currVideo?._id)}
-          >
-            {isSaved ? (
-              <MdWatchLater className="text-white text-2xl md:text-4xl cursor-pointer" />
-            ) : (
-              <MdOutlineWatchLater className="text-white text-2xl md:text-4xl cursor-pointer" />
-            )}
-          </button>
+        <div className="bg-gray-900 flex justify-between items-center  px-2 py-3 ">
+          <div className="flex items-center">
+            <button
+              onClick={() => currVideo?._id && likeHandler(currVideo?._id)}
+            >
+              {isVideoLiked ? (
+                <AiFillLike className="text-white text-2xl md:text-4xl cursor-pointer" />
+              ) : (
+                <AiOutlineLike className="text-white text-2xl md:text-4xl cursor-pointer" />
+              )}
+            </button>
+          </div>
+          <div className="flex items-center">
+            <button
+              onClick={() => currVideo?._id && watchHandler(currVideo?._id)}
+            >
+              {isSaved ? (
+                <MdWatchLater className="text-white text-2xl md:text-4xl cursor-pointer" />
+              ) : (
+                <MdOutlineWatchLater className="text-white text-2xl md:text-4xl cursor-pointer" />
+              )}
+            </button>
+          </div>
+          <div className="flex items-center justify-center">
+            <CgPlayListAdd
+              className="text-white text-2xl md:text-4xl cursor-pointer"
+              onClick={() => setShowModal(true)}
+            />
+          </div>
         </div>
-        <div className="flex items-center justify-center">
-          <CgPlayListAdd
-            className="text-white text-2xl md:text-4xl cursor-pointer"
-            onClick={() => setShowModal(true)}
-          />
-        </div>
-      </div>
-      {showModal && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-md">
-            {/* <h3 className="text-lg font-bold mb-4">Add to Playlist</h3> */}
-            {/* <select
+        {showModal && (
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-md">
+              {/* <h3 className="text-lg font-bold mb-4">Add to Playlist</h3> */}
+              {/* <select
               className="border border-gray-300 rounded-md p-2 mb-4"
               onChange={(e) => addToPlaylist(e.target.value)}
             >
@@ -173,45 +177,47 @@ const Video = () => {
                 </option>
               ))}
             </select> */}
-            <div className="border border-gray-300 rounded-md p-2 mb-4">
-              <p>Select an existing playlist:</p>
-              {playlists?.map((item) => (
-                <div key={item._id}>
-                  <input
-                    type="checkbox"
-                    id={item._id}
-                    value={item._id}
-                    onChange={(e) => addToPlaylist(e.target.value)}
-                  />
-                  <label htmlFor={item._id}>{item.name}</label>
-                </div>
-              ))}
-            </div>
-            <div>
-              <input
-                type="text"
-                className="border border-gray-300 rounded-md p-2 mb-4"
-                placeholder="Enter a new playlist name"
-                value={newPlaylistName}
-                onChange={(e) => setNewPlaylistName(e.target.value)}
-              />
+              <div className="border border-gray-300 rounded-md p-2 mb-4">
+                <p>Select an existing playlist:</p>
+                {playlists?.map((item) => (
+                  <div key={item._id}>
+                    <input
+                      type="checkbox"
+                      id={item._id}
+                      value={item._id}
+                      onChange={(e) => addToPlaylist(e.target.value)}
+                    />
+                    <label htmlFor={item._id}>{item.name}</label>
+                  </div>
+                ))}
+              </div>
+              <div>
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded-md p-2 mb-4"
+                  placeholder="Enter a new playlist name"
+                  value={newPlaylistName}
+                  onChange={(e) => setNewPlaylistName(e.target.value)}
+                />
+                <button
+                  className="bg-red-400 text-white rounded-md px-4 py-2"
+                  onClick={createPlaylist}
+                >
+                  Create New Playlist
+                </button>
+              </div>
               <button
-                className="bg-red-400 text-white rounded-md px-4 py-2"
-                onClick={createPlaylist}
+                className="bg-gray-200 text-gray-800 rounded-md px-4 py-2 mt-4"
+                onClick={() => setShowModal(false)}
               >
-                Create New Playlist
+                Cancel
               </button>
             </div>
-            <button
-              className="bg-gray-200 text-gray-800 rounded-md px-4 py-2 mt-4"
-              onClick={() => setShowModal(false)}
-            >
-              Cancel
-            </button>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      <div>{id && <Recommendation id={id} />}</div>
+    </>
   );
 };
 
