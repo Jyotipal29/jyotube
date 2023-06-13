7745e7511b8a4681113e97ce597fcfaaf5ebd82d
jyoti pal <paljyoti0129@gmail.com>
2023-06-13 19:16:03 +0530

    created clear history routes

--- backend/routes/historyRoutes.js
+++ backend/routes/historyRoutes.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 const { protect } = require("./verifyToken");
-const { addHistory, getHistory } = require("../controller/historyController");
+const {
+  addHistory,
+  getHistory,
+  clearHistory,
+} = require("../controller/historyController");
 
 router.post("/:id", protect, addHistory);
 router.get("/", protect, getHistory);
+router.delete("/", protect, clearHistory);
 
 module.exports = router;
