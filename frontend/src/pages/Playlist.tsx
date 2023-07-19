972e22a95cd3fc8fee5db5d04ca530f01366aae5
jyoti pal <paljyoti0129@gmail.com>
2023-07-19 11:21:32 +0530

    style updated

--- frontend/src/pages/Playlist.tsx
+++ frontend/src/pages/Playlist.tsx
@@ -5,10 +5,22 @@ import { useUser } from "../context/userContext/userContext";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Layout from "../component/Layout";
-import { RotatingLines } from "react-loader-spinner";
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import { RotatingLines } from "react-loader-spinner";
+type state = {
+  id: null | string;
+  isLoading: boolean;
+};
 const Playlist = () => {
   const [loading, setLoading] = useState(false);
+  const [playLoading, setPLayLoading] = useState<state>({
+    id: null,
+    isLoading: false,
+  });
+  const [videoLoading, setVideoLoading] = useState<state>({
+    id: null,
+    isLoading: false,
+  });
 
   const {
     userState: { user },
@@ -34,6 +46,10 @@ const Playlist = () => {
   }, []);
 
   const deletePlaylist = async (id: string) => {
+    setPLayLoading({
+      isLoading: true,
+      id,
+    });
     const config = {
       headers: {
         Authorization: `Bearer ${user?.token}`,
@@ -41,10 +57,15 @@ const Playlist = () => {
     };
     await axios.delete(`${api}playlist/${id}`, config);
     videoDispatch({ type: "DELETE_PLAYLIST", payload: id });
+    setPLayLoading({
+      isLoading: false,
+      id: null,
+    });
   };
 
   const deleteVideo = async (playlistId: string, videoId: string) => {
     console.log(playlistId, videoId, "the ids to delete");
+    setVideoLoading({ isLoading: true, id: videoId });
     const config = {
       headers: {
         Authorization: `Bearer ${user?.token}`,
@@ -58,6 +79,7 @@ const Playlist = () => {
         videoId,
       },
     });
+    setVideoLoading({ isLoading: false, id: null });
   };
 
   console.log(playlists, "this is playlist");
@@ -75,11 +97,30 @@ const Playlist = () => {
         </div>
       ) : (
         playlists.map((items) => (
-          <div>
-            <div className="flex justify-between px-12 bg-gray-600">
-              <button onClick={() => deletePlaylist(items._id)}>delete</button>
+          <div className="py-5">
+            <div className="flex  justify-between px-12 shadow-md shadow-gray-600 ">
+              <h1 className=" py-2 text-4xl tracking-widest uppercase text-center text-red-600">
+                {items.name}
+              </h1>
 
-              <p className=" py-2  text-center">{items.name}</p>
+              <button
+                className="text-red-600"
+                onClick={() => deletePlaylist(items._id)}
+              >
+                {playLoading?.id === items._id && playLoading?.isLoading ? (
+                  <div className="flex items-center justify-center">
+                    <RotatingLines
+                      strokeColor="red"
+                      strokeWidth="5"
+                      animationDuration="0.75"
+                      width="20"
+                      visible={true}
+                    />
+                  </div>
+                ) : (
+                  <AiOutlineCloseCircle className="text-red-600 text-3xl cursor-pointer" />
+                )}
+              </button>
             </div>
 
             <div className="grid  mt-5 grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
@@ -95,10 +136,22 @@ const Playlist = () => {
                     </Link>
                   </div>
                   <div className="absolute top-0 right-3  ">
-                    <AiOutlineCloseCircle
-                      className="text-red-600 text-3xl cursor-pointer"
-                      onClick={() => deleteVideo(items._id, item._id)}
-                    />
+                    <button onClick={() => deleteVideo(items._id, item._id)}>
+                      {videoLoading?.id === item._id &&
+                      videoLoading?.isLoading ? (
+                        <div className="flex items-center justify-center">
+                          <RotatingLines
+                            strokeColor="red"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="20"
+                            visible={true}
+                          />
+                        </div>
+                      ) : (
+                        <AiOutlineCloseCircle className="text-red-600 text-3xl cursor-pointer" />
+                      )}
+                    </button>
                   </div>
 
                   <div className="flex justify-start px-2 space-x-2">
